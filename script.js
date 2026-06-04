abd7c6217d29aa3f05e5481102b547e56b47d194
hemm-hub <matheushenriquevp@gmail.com>
2026-06-04 18:42:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function comprar(produto){
+
+document
+.getElementById("produto")
+.value = produto;
+
+window.scrollTo({
+top: document.querySelector(".pedido").offsetTop,
+behavior:"smooth"
+});
+
+}
