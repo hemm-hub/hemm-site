0762a7b4ecfd9000c35e616c307626a0b150da1a
hemm-hub <companyhemm@gmail.com>
2026-06-05 13:24:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function comprar(produto){
 
-document
-.getElementById("produto")
-.value = produto;
+document.getElementById("produto").value = produto;
 
 window.scrollTo({
 top: document.querySelector(".pedido").offsetTop,
-behavior:"smooth"
+behavior: "smooth"
 });
 
 }
